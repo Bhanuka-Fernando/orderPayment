9e690a494c80693e4607dc1c2426353cdf48c202
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2025-04-19 10:25:29 +0530

    created order model

---
+++ src/main/java/com/fooddelivery/orderpayment/model/Order.java
@@ -0,0 +1,25 @@
+package com.fooddelivery.orderpayment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "orders")
+public class Order {
+    @Id
+    private String id;
+    private String customerId;
+    private List<OrderItem> items;
+    private double totalPrice;
+    private String status;
+    private LocalDateTime createdAt;
+
+}
