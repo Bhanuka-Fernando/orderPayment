28a91b33208b1f67ef89b5014d5f575b795db338
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2025-04-23 00:46:21 +0530

    cc

--- src/main/java/com/fooddelivery/orderpayment/services/StripeService.java
+++ src/main/java/com/fooddelivery/orderpayment/services/StripeService.java
@@ -12,18 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class StripeService {
 
-//    @Value("${stripe.secretKey}")
+    @Value("${stripe.secretKey}")
     private String secretKey;
 
     public StripeResponse checkoutProducts(ProductRequest productRequest){
-//         Stripe.apiKey = secretKey;
+         Stripe.apiKey = secretKey;
 
         SessionCreateParams.LineItem.PriceData.ProductData productData =  new SessionCreateParams.LineItem.PriceData.ProductData.Builder()
                 .setName(productRequest.getName()).build();
 
         SessionCreateParams.LineItem.PriceData priceData = new SessionCreateParams.LineItem.PriceData.Builder()
                 .setCurrency(productRequest.getCurrency()==null?"USD": productRequest.getCurrency())
-                .setUnitAmount(productRequest.getAmount())
+                .setUnitAmount(productRequest.getAmount()*100)
                 .setProductData(productData)
                 .build();
 
@@ -36,7 +36,7 @@ public class StripeService {
 
         SessionCreateParams params = SessionCreateParams.builder()
                 .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl("http://localhost:8080/success")  // success navigate to this endpoint
+                .setSuccessUrl("http://localhost:3000/cart")  // success navigate to this endpoint
                 .setCancelUrl("http://localhost:8080/cancel")
                 .addLineItem(lineItem)
                 .build();
